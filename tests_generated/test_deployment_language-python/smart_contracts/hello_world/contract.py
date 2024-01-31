c9acc0c1a6026b443f3d49df5c75245a2db590d4
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-01-31 11:27:37 +0800

    chore: remove unused approvals

--- tests_generated/test_deployment_language-python/smart_contracts/hello_world/contract.py
+++
@@ -1,8 +0,0 @@
-from puyapy import ARC4Contract, Bytes
-from puyapy.arc4 import String, abimethod
-
-
-class HelloWorld(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return String.encode(Bytes(b"Hello ") + name.decode())
