60700af4836c81cea193ac5201f9ca8ba7ea0821
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2024-10-15 17:59:08 +0200

    feat: migrating to latest beta prereleases of utils, utils-debug and generator

--- examples/generators/production_python_smart_contract_typescript/tests/hello-world.spec.ts
+++ examples/generators/production_python_smart_contract_typescript/tests/hello-world.spec.ts
@@ -1,7 +1,8 @@
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing'
-import { HelloWorldClient } from '../smart_contracts/artifacts/hello_world/HelloWorldClient'
+import { HelloWorldFactory } from '../smart_contracts/artifacts/hello_world/HelloWorldClient'
 import { Account, Algodv2, Indexer } from 'algosdk'
 import * as algokit from '@algorandfoundation/algokit-utils'
+import { TransactionSignerAccount } from '@algorandfoundation/algokit-utils/types/account'
 
 describe('hello world contract', () => {
   const localnet = algorandFixture()
@@ -13,39 +14,36 @@ describe('hello world contract', () => {
   })
   beforeEach(localnet.beforeEach)
 
-  const deploy = async (account: Account, algod: Algodv2, indexer: Indexer) => {
-    const client = new HelloWorldClient(
-      {
-        resolveBy: 'creatorAndName',
-        findExistingUsing: indexer,
-        sender: account,
-        creatorAddress: account.addr,
-      },
-      algod,
-    )
-    await client.deploy({
-      onSchemaBreak: 'append',
-      onUpdate: 'append',
+  const deploy = async (account: Account & TransactionSignerAccount) => {
+    const factory = localnet.algorand.client.getTypedAppFactory(HelloWorldFactory, {
+      defaultSender: account.addr,
+      defaultSigner: account.signer,
     })
-    return { client }
+
+    const { appClient } = await factory.deploy({ onUpdate: 'append', onSchemaBreak: 'append' })
+    return { client: appClient }
   }
 
   test('says hello', async () => {
-    const { algod, indexer, testAccount } = localnet.context
-    const { client } = await deploy(testAccount, algod, indexer)
+    const { testAccount } = localnet.context
+    const { client } = await deploy(testAccount)
 
-    const result = await client.hello({ name: 'World' })
+    const result = await client.send.hello({ args: { name: 'World' } })
 
     expect(result.return).toBe('Hello, World')
   })
 
   test('simulate says hello with correct budget consumed', async () => {
-    const { algod, indexer, testAccount } = localnet.context
-    const { client } = await deploy(testAccount, algod, indexer)
-    const result = await client.compose().hello({ name: 'World' }).hello({ name: 'Jane' }).simulate()
+    const { testAccount } = localnet.context
+    const { client } = await deploy(testAccount)
+    const result = await client
+      .newGroup()
+      .hello({ args: { name: 'World' } })
+      .hello({ args: { name: 'Jane' } })
+      .simulate()
 
-    expect(result.methodResults[0].returnValue).toBe('Hello, World')
-    expect(result.methodResults[1].returnValue).toBe('Hello, Jane')
+    expect(result.returns[0]).toBe('Hello, World')
+    expect(result.returns[1]).toBe('Hello, Jane')
     expect(result.simulateResponse.txnGroups[0].appBudgetConsumed).toBeLessThan(100)
   })
 })
